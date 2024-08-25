c4aabc0f10d21342269e2933c75f32f05909a91b
Lucas Micheli <hello@lucasmicheli.com>
2024-08-25 19:56:57 -0300

    refactor: Update loadImage function to use ES6 syntax
    The `loadImage` function in `main.js` has been updated to use ES6 arrow function syntax and `const` and `let` declarations instead of `var`. This improves code readability and adheres to modern JavaScript standards.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-function loadImage(id, targetId) {
-    var el = document.getElementById(id);
-    var targetEl = targetId ? document.getElementById(targetId) : el;
-    var imageToLoad;
+const loadImage = (id, targetId) => {
+    const el = document.getElementById(id);
+    const targetEl = targetId ? document.getElementById(targetId) : el;
+    let imageToLoad;
 
     if (el.dataset.image) {
         imageToLoad = el.dataset.image;
@@ -12,7 +12,7 @@ function loadImage(id, targetId) {
     }
 
     if (imageToLoad) {
-        var img = new Image();
+        const img = new Image();
         img.src = imageToLoad;
         img.onload = function() {
             targetEl.classList.add('is-loaded');
